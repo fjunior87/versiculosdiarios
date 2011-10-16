e018a34201620ab18e63d0a86ca41c12e9f4a5f0
Junior Ribeiro <fjunior87@gmail.com>
2011-10-16 10:38:15 -0300

    Added logic for processing the verses from arca universal

--- logic.py
+++ logic.py
@@ -1,9 +1,10 @@
+﻿#-*- encode: utf-8 -*-
 from google.appengine.api import users
 import models
 
 BIBLIA_ONLINE = "bibliaonline"
 FONTE_A_JORRAR = "fonteajorrar"
-
+ARCA_UNIVERSAL = "arcauniversal"
 
 class YqlProcessor():
 	result = None
@@ -37,12 +38,42 @@ def process_results_biblia_online(result):
 		
 		return verse
 
-def process_results_fonte_a_jorrar(result):
-	pass
+def process_results_arca_universal(result):
+	import urllib
+	import re
+	source = "Arca Universal"
+	source_link = "http://www.arcauniversal.com/servicos/caixadepromessas.html"
+				
+	versePattern = "<h4>(.*)</h4>"
+	bookPattern = "<span class=\"style1\">(.*)</span>"
+	
+	if result.content:
+		requestContent = result.content
+		verseRegex = re.search(versePattern,requestContent)
+		bookRegex = re.search(bookPattern,requestContent)
+		if verseRegex and bookRegex:
+		
+			verseContent = verseRegex.group(1).decode("utf8")
+			book = bookRegex.group(1).replace(".",":").decode("utf8")
+			title = book
+			
+			verse = models.Verse(
+				content = verseContent,
+				book = book,
+				sourceLink = source_link,
+				title = title,
+				user = users.get_current_user(),
+				source = source
+			)
+			verse.put()
+			
+			return verse
+	
+	
 
 process_strategy= {
 	BIBLIA_ONLINE: process_results_biblia_online,
-	FONTE_A_JORRAR: process_results_fonte_a_jorrar
+	ARCA_UNIVERSAL: process_results_arca_universal
 }
 	
 		
